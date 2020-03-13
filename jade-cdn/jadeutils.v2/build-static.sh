52fb05ab212817469f6406db75d7d8e73dab462b
Jade Shan <evokeralucard@gmail.com>
2020-03-13 12:29:28 -0400

    update

--- jade-cdn/jadeutils.v2/build-static.sh
+++ jade-cdn/jadeutils.v2/build-static.sh
@@ -8,27 +8,21 @@ while getopts "b:ctrae" arg #选项后面的冒号表示该选项需要参数
 do
 	case $arg in
 		c)
-			gulp clean       ;
-			sleep 5 ;
-			gulp check-js    ;
-#			gulp build-less-base  ;
-#			gulp build-less-hobbit  ;
-			sleep 5 ;
-#			gulp min-styles-base  ;
-#			gulp min-styles-hobbit  ;
-			gulp min-scripts ;
+			gulp clean-style ;
+			sleep 3 ;
+			gulp build-less  ;
+			sleep 3 ;
+			gulp min-styles  ;
+			sleep 3 ;
+# 			gulp check-js    ;
+# #			gulp build-less-base  ;
+# #			gulp build-less-hobbit  ;
+# 			sleep 5 ;
+# #			gulp min-styles-base  ;
+# #			gulp min-styles-hobbit  ;
+# 			gulp min-scripts ;
 			;;
 		a)
-			gulp clean       ;
-			sleep 5 ;
-			gulp check-js    ;
-#			gulp build-less-base  ;
-#			gulp build-less-hobbit  ;
-			sleep 5 ;
-#			gulp min-styles-base  ;
-#			gulp min-styles-hobbit  ;
-			gulp min-scripts ;
-			sleep 5 ;
 			# qrsync ~/.config/qiniu/jadeutils.v2.json;
 			;;
 		?)  #当有不认识的选项的时候arg为?
--- jade-cdn/jadeutils.v2/gulpfile.js
+++ jade-cdn/jadeutils.v2/gulpfile.js
@@ -5,50 +5,88 @@
 // gulp build-less：你会在目录下发现less目录下的less文件被编译成对应的css文件。
 // gulp min-styles：会在css目录下输出all.css和all.min.css文件。
 // gulp develop：会监听所有less文件，当有less文件改变时，会执行build-less和min-styles
-var gulp = require('gulp'),
-		less = require('gulp-less'),              //less编译
-		minifycss = require('gulp-minify-css'),   //css压缩
-		jshint = require('gulp-jshint'),          //js检查
-		uglify  = require('gulp-uglify'),         //js压缩
-		rename = require('gulp-rename'),          //重命名
-		concat  = require('gulp-concat'),         //合并文件
-		clean = require('gulp-clean');            //清空文件夹
-		
-var pathSrcScripts = "./src/javascript/";
+const gulp      = require('gulp');
+const less      = require('gulp-less');        //less编译
+const minifycss = require('gulp-minify-css');  //css压缩
+const jshint    = require('gulp-jshint');      //js检查
+const uglify    = require('gulp-uglify');      //js压缩
+const rename    = require('gulp-rename');      //重命名
+const concat    = require('gulp-concat');      //合并文件
+const clean     = require('gulp-clean');       //清空文件夹
 
-var pathOutputScripts = "./web-root/scripts/";
+const srcStylePath =      'src/css-themes/hobbit/styles/';
+const tmpStylePath =      'tmp/css-themes/hobbit/styles/';
+const tagStylePath = 'web-root/css-themes/hobbit/styles/';
 
-// 清空图片、样式、js
-gulp.task('clean', function() {
-		gulp.src([pathOutputScripts], 
-			{read: false}).pipe(clean());
+const srcScriptPath =      'src/scripts/';
+const tmpScriptPath =      'tmp/scripts/';
+const tagScriptPath = 'web-root/scripts/';
+
+
+var pathSrcScripts = './src/javascript/';
+var pathOutputScripts = './web-root/scripts/';
+
+// =======================
+// css
+// =======================
+
+gulp.task('clean-styles', async (callback) => {
+		await gulp.src([tmpStylePath, tagStylePath], 
+			{read: false, allowEmpty: true}).pipe(clean());
+		await callback();
 });
 
-// 检查javascript
-gulp.task('check-js', function() {
-	gulp.src(pathSrcScripts + '**/*.js').pipe(jshint()) .pipe(jshint.reporter('default'));
-	});
+gulp.task('build-less', gulp.series(/*'clean-styles',*/ async (callback) => {
+	await gulp.src(srcStylePath + '**/*.less')
+		.pipe(less({compress: true}))
+		.on('error', (e) => {console.log(e)})
+		.pipe(gulp.dest(tmpStylePath));
+	await callback();
+}));
 
-gulp.task('min-scripts', function() {
-	gulp.src([pathSrcScripts + 'base.js', pathSrcScripts + 'dataStructure.js',
-		pathSrcScripts + 'instance.js'
-	])
-		.pipe(concat('jadeutils.v2.js'))
-		.pipe(gulp.dest(pathOutputScripts))
-		.pipe(rename({suffix: '.min'}))
-		.pipe(uglify())
-		.pipe(gulp.dest(pathOutputScripts));
-	});
+gulp.task('min-styles', gulp.series(/* 'build-less',*/ async (callback) => {
+	await gulp.src([tmpStylePath + '*.css'])
+		.pipe(concat('all.css'))           // 合并文件为all.css
+		.pipe(gulp.dest(tmpStylePath))     // 输出all.css文件
+		.pipe(rename({suffix: '.min'}))    // 重命名all.css为 all.min.css
+		.pipe(minifycss())                 // 压缩css文件
+		.pipe(gulp.dest(tagStylePath));    // 输出all.min.css
+	await callback();
+}));
 
-// // 默认任务 清空图片、样式、js并重建 运行语句 gulp
-// gulp.task('default', ['build-less'], function(){
-//     gulp.start('min-styles','min-scripts');
-//     gulp.start();
+
+
+// // 清空图片、样式、js
+// gulp.task('clean', function() {
+// 		gulp.src([pathOutputScripts], 
+// 			{read: false}).pipe(clean());
 // });
-// 监控变化
-gulp.task('develop', function() {
-	gulp.watch(
-		[ pathSrcScripts + '**/*.js'], 
-		['clean']);
-		});
-
+// 
+// // 检查javascript
+// gulp.task('check-js', function() {
+// 	gulp.src(pathSrcScripts + '**/*.js').pipe(jshint()) .pipe(jshint.reporter('default'));
+// 	});
+// 
+// gulp.task('min-scripts', function() {
+// 	gulp.src([pathSrcScripts + 'base.js', pathSrcScripts + 'dataStructure.js',
+// 		pathSrcScripts + 'instance.js'
+// 	])
+// 		.pipe(concat('jadeutils.v2.js'))
+// 		.pipe(gulp.dest(pathOutputScripts))
+// 		.pipe(rename({suffix: '.min'}))
+// 		.pipe(uglify())
+// 		.pipe(gulp.dest(pathOutputScripts));
+// 	});
+// 
+// // // 默认任务 清空图片、样式、js并重建 运行语句 gulp
+// // gulp.task('default', ['build-less'], function(){
+// //     gulp.start('min-styles','min-scripts');
+// //     gulp.start();
+// // });
+// // 监控变化
+// gulp.task('develop', function() {
+// 	gulp.watch(
+// 		[ pathSrcScripts + '**/*.js'], 
+// 		['clean']);
+// 		});
+// 
