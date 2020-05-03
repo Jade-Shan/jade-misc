bc4bdb42b349387992c9bc57bd17b19944c0f5f2
Jade Shan <evokeralucard@gmail.com>
2020-05-03 14:15:52 +0800

    update

--- fortunes/build.sh
+++ fortunes/build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 rm -rf out
-mkdir out
-find *.txt -exec cp {} out/{} \;
-cd out
-rename 's/\.txt$//' *.txt
-find * -exec strfile {} \;
-sudo cp ./* /usr/share/games/fortunes 
+cp -r src out
+rename 's/\.txt$//' out/*.txt
+find out/* -exec strfile {} \;
+# find src/*.txt -exec cp {} out/{} \;
+# sudo cp out/*.dat /usr/share/games/fortunes 
+sudo cp out/* /usr/share/games/fortunes 
