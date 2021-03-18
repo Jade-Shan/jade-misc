d328614aa5b342f4e5dc098c151899b1952ddae1
Jade Shan <evokeralucard@gmail.com>
2021-03-18 16:30:59 +0800

    update blog css

--- jade-cdn/src/jadeutils.v2/build.sh
+++ jade-cdn/src/jadeutils.v2/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo '--- Starting build : jadeutils.v2 ---'
 gulp
-rm -rf ../../webroot/jadeutils.v2
-cp -r webroot ../../webroot/jadeutils.v2
+rm -rf ../../webroot/jadeutils.v2/*
+cp -r webroot/* ../../webroot/jadeutils.v2
 echo '--- finish build : jadeutils.v2 ---'
 
