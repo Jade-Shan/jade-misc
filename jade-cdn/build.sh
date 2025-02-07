567affb5d00ad58da500a1e101bd74e1c902d226
Jade Shan <evokeralucard@gmail.com>
2025-02-07 22:31:00 +0800

    update

--- jade-cdn/build.sh
+++ jade-cdn/build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-cd src/3rd.v2/
-bash ./build.sh
-cd ../..
+# cd src/3rd.v2/
+# bash ./build.sh
+# cd ../..
 
 cd src/jadeutils.v2/
 bash ./build.sh
--- jade-cdn/src/jadeutils.v2/build.sh
+++ jade-cdn/src/jadeutils.v2/build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+mkdir -p webroot  ../../webroot/jadeutils.v2
 echo '--- Starting build : jadeutils.v2 ---'
-gulp
+node ./node_modules/gulp-cli/bin/gulp.js
 rm -rf ../../webroot/jadeutils.v2/*
 cp -r webroot/* ../../webroot/jadeutils.v2
 echo '--- finish build : jadeutils.v2 ---'
